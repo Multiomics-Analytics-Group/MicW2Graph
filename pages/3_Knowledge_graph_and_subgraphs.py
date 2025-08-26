920ea142a7af8a2c3066c899a4c15ae3581b859c
sayalaruano <sebasar1245@gmail.com>
2025-08-26 13:51:56 +0200

    🐛 Fix(pages/3_Knowledge_graph_and_subgraphs.py): Replace st.stop() with flag to render KG to avoid issues on the hosted web app

--- pages/3_Knowledge_graph_and_subgraphs.py
+++ pages/3_Knowledge_graph_and_subgraphs.py
@@ -75,10 +75,11 @@ with col2:
         "<h3 style='text-align: center; color: black;'>MicW2Graph subgraphs</h3>",
         unsafe_allow_html=True,
     )
+
     subcol1, subcol2 = st.columns(2)
 
+    # Selectboxes to choose the sub-biome and experiment type
     with subcol1:
-        # Selectbox to choose the sub-biome to explore
         sub_biome = st.selectbox(
             " ",
             [
@@ -93,7 +94,6 @@ with col2:
         )
 
     with subcol2:
-        # Selectbox to choose the experiment type
         exp_type = st.selectbox(
             " ",
             ["Metagenomics", "Metatranscriptomics", "Assembly"],
@@ -102,114 +102,130 @@ with col2:
             label_visibility="collapsed",
         )
 
-    if sub_biome == None or exp_type == None:
+    # Use a flag to control rendering of the graph
+    render_graph = False
+    if sub_biome and exp_type:
+        # Replace spaces with underscores
+        sub_biome = sub_biome.replace(" ", "_")
+
+        try:
+            # Get the subgraph path from the config file
+            kg_subgraph_path = st.session_state.config["knowledge_graphs"][sub_biome][
+                exp_type
+            ]
+
+            # Add the .graphml extension to the path
+            kg_subgraph_graphml = kg_subgraph_path + ".graphml"
+
+            # Get the node and edge colors from the config file
+            color_nodes = st.session_state.config["kg_nodes"]
+            edge_colors = st.session_state.config["kg_edges"]
+
+            # Load and preprocess the network data
+            G = web_utils.load_and_preprocess_kg(
+                kg_subgraph_graphml, color_nodes, edge_colors
+            )
+            render_graph = True
+        except KeyError:
+            # Catch specific error for more informative message
+            st.warning(
+                "The KG of the sub-biome and experiment type is not aviáilable. Select another combination.",
+                icon="⚠️",
+            )
+        except FileNotFoundError:
+            # Catch specific error for more informative message
+            st.warning(
+                "The knowledge graph file could not be found. Please select another sub-biome and experiment type.",
+                icon="⚠️",
+            )
+        except Exception as e:
+            # Catch other exceptions
+            st.warning(f"An unexpected error occurred: {e}", icon="⚠️")
+    else:
         st.warning("Please select a sub-biome and experiment type.", icon="⚠️")
-        st.stop()
 
-    # Replace spaces with underscores
-    sub_biome = sub_biome.replace(" ", "_")
-
-    try:
-        # Get the subgraph path from the config file
-        kg_subgraph_path = st.session_state.config["knowledge_graphs"][sub_biome][
-            exp_type
-        ]
-
-        # Add the .graphml extension to the path
-        kg_subgraph_graphml = kg_subgraph_path + ".graphml"
-
-        # Get the node and edge colors from the config file
-        color_nodes = st.session_state.config["kg_nodes"]
-        edge_colors = st.session_state.config["kg_edges"]
-
-        # Load and preprocess the network data
-        G = web_utils.load_and_preprocess_kg(
-            kg_subgraph_graphml, color_nodes, edge_colors
-        )
-    except:
-        st.warning(
-            "The knowledge graph is not available. Please select another sub-biome and experiment type.",
-            icon="⚠️",
-        )
-        st.stop()
-
-    # Initiate PyVis network object
-    net = Network(height="600px", width="100%", bgcolor="white", font_color="#555555")
-
-    # Take Networkx graph and translate it to a PyVis graph format
-    net.from_nx(G)
-
-    # Show number of nodes and edges
-    st.markdown(
-        f"<p style='text-align: center; color: black;'> <b>Number of nodes:</b> {len(net.nodes)} </p>",
-        unsafe_allow_html=True,
-    )
-    st.markdown(
-        f"<p style='text-align: center; color: black;'> <b>Number of relationships:</b> {len(net.edges)} </p>",
-        unsafe_allow_html=True,
-    )
-
-    # Modify nodes to add custom labels, titles, border widths, and colors
-    for node in net.nodes:
-        node_id = node["id"]
-        node_data = G.nodes[node_id]
-        node["font"] = {"size": 12}
-        label = node_data.get("name")
-        node["label"] = label
-        # node['title'] = label  # Tooltip text
-        node_data.get("name", node_id)  # Tooltip text
-        node["borderWidth"] = 1.5
-        node["borderWidthSelected"] = 2
-        node["color"] = {
-            "background": node_data["color"],
-            "border": node_data["border-color"],
-        }
-
-    # Modify edges to add custom colors and labels
-    for edge in net.edges:
-        u, v = edge["from"], edge["to"]
-        edge_data = G.get_edge_data(u, v)
-        edge["color"] = edge_data["color"]
-        edge["font"] = {"size": 6}
-
-    # Streamlit checkbox for physics interactivity
-    control_layout = st.checkbox("Add panel to control layout", value=False)
-
-    # Conditionally apply the force_atlas_2based layout based on checkbox
-    if control_layout:
-        net.force_atlas_2based(
-            gravity=-50,
-            central_gravity=0.005,
-            spring_length=100,
-            spring_strength=0.08,
-            damping=0.4,
-        )
-        net.show_buttons(filter_=["physics"])
-    else:
-        net.force_atlas_2based(
-            gravity=-50,
-            central_gravity=0.005,
-            spring_length=100,
-            spring_strength=0.08,
-            damping=0.4,
+    # Only render the graph and related components if the data was loaded successfully
+    if render_graph:
+        # Initiate PyVis network object
+        net = Network(
+            height="600px", width="100%", bgcolor="white", font_color="#555555"
         )
 
-    # Add the .html extension to the path
-    kg_subgraph_html = kg_subgraph_path + ".html"
+        # Take Networkx graph and translate it to a PyVis graph format
+        net.from_nx(G)
 
-    # Save network as HTML
-    net.save_graph(kg_subgraph_html)
+        # Show number of nodes and edges
+        st.markdown(
+            f"<p style='text-align: center; color: black;'> <b>Number of nodes:</b> {len(net.nodes)} </p>",
+            unsafe_allow_html=True,
+        )
+        st.markdown(
+            f"<p style='text-align: center; color: black;'> <b>Number of relationships:</b> {len(net.edges)} </p>",
+            unsafe_allow_html=True,
+        )
 
-    # Load HTML data
-    html_data = utils.load_html_file(kg_subgraph_html)
+        # Modify nodes to add custom labels, titles, border widths, and colors
+        for node in net.nodes:
+            node_id = node["id"]
+            node_data = G.nodes[node_id]
+            node["font"] = {"size": 12}
+            label = node_data.get("name")
+            node["label"] = label
+            # node['title'] = label  # Tooltip text
+            node_data.get("name", node_id)  # Tooltip text
+            node["borderWidth"] = 1.5
+            node["borderWidthSelected"] = 2
+            node["color"] = {
+                "background": node_data["color"],
+                "border": node_data["border-color"],
+            }
 
-    if control_layout:
-        # Load HTML into HTML component for display on Streamlit
-        components.html(html_data, height=1100)
-    else:
-        # Load HTML into HTML component for display on Streamlit
-        components.html(html_data, height=600)
+        # Modify edges to add custom colors and labels
+        for edge in net.edges:
+            u, v = edge["from"], edge["to"]
+            edge_data = G.get_edge_data(u, v)
+            edge["color"] = edge_data["color"]
+            edge["font"] = {"size": 6}
 
+        # Streamlit checkbox for physics interactivity
+        control_layout = st.checkbox("Add panel to control layout", value=False)
+
+        # Conditionally apply the force_atlas_2based layout based on checkbox
+        if control_layout:
+            net.force_atlas_2based(
+                gravity=-50,
+                central_gravity=0.005,
+                spring_length=100,
+                spring_strength=0.08,
+                damping=0.4,
+            )
+            net.show_buttons(filter_=["physics"])
+        else:
+            net.force_atlas_2based(
+                gravity=-50,
+                central_gravity=0.005,
+                spring_length=100,
+                spring_strength=0.08,
+                damping=0.4,
+            )
+
+        # Add the .html extension to the path
+        kg_subgraph_html = kg_subgraph_path + ".html"
+
+        # Save network as HTML
+        net.save_graph(kg_subgraph_html)
+
+        # Load HTML data
+        html_data = utils.load_html_file(kg_subgraph_html)
+
+        if control_layout:
+            # Load HTML into HTML component for display on Streamlit
+            components.html(html_data, height=1100)
+        else:
+            # Load HTML into HTML component for display on Streamlit
+            components.html(html_data, height=600)
+
+    # Download buttons for the subgraph
     c1, c2, c3 = st.columns(3)
 
     with c1:
